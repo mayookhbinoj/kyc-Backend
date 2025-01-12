8d20d01d25080ef0ae8df4b44cff1bf4ddf0620d
mayookh <mayookhbino77@gmail.com>
2025-01-12 10:32:10 +0530

    demo request

--- src/route/userRoute.ts
+++ src/route/userRoute.ts
@@ -5,7 +5,9 @@ import { authorize } from "../middleware/authMiddleware";
 
 const userRoute = () => {
   const router = express.Router();
-
+  router.get("/",(req,res)=>{
+    res.json({helo:"message"})
+  })
   router.post("/register", userRegister);
   router.post("/userLogin", userLogin);
   router.post("/kycSubmit", authorize(['User']), upload, kycSubmit);
