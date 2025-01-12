35cab8d3ba1eb7668d4817c9e44b0aaa01138aa0
mayookh <mayookhbino77@gmail.com>
2025-01-12 14:58:19 +0530

    base url changed

--- src/webserver/expressConfig.ts
+++ src/webserver/expressConfig.ts
@@ -6,7 +6,7 @@ import path from "path"
 
 const expressconfig=(app:Application)=>{
     const corsConfig = {
-        origin: '', 
+        origin: 'https://kyc-frontend-ebon.vercel.app', 
         credentials: true, 
     }
     app.use(cors(corsConfig))
